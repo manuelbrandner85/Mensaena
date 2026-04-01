f66b604e2f16ad41de3848e981e5a5a1491227d0
manuelbrandner85 <genspark_dev@genspark.ai>
2026-04-01 16:54:40 +0000

    remove: Dark Mode vollständig entfernt (Toggle, CSS-Variablen, Komponente)

--- .open-next/cloudflare/init.js
+++ .open-next/cloudflare/init.js
@@ -49,7 +49,7 @@ function initRuntime() {
   };
   Object.assign(globalThis, {
     Request: CustomRequest,
-    __BUILD_TIMESTAMP_MS__: 1775061599655,
+    __BUILD_TIMESTAMP_MS__: 1775062431152,
     __NEXT_BASE_PATH__: "",
     __ASSETS_RUN_WORKER_FIRST__: false,
     __TRAILING_SLASH__: false,
